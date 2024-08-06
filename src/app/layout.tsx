1bbe8e3a9cba917edd6c2aaf43fdebdbc4a36627
MonsteRico <boilermonster0530@yahoo.com>
2024-08-05 22:54:35 -0400

    viewport changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Providers } from "@/components/Providers";
 import { TopNav } from "@/components/TopNav";
 import { Toaster } from "@/components/ui/sonner";
 import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
 
 import { Inter } from "next/font/google";
 
@@ -10,12 +11,19 @@ const inter = Inter({
   variable: "--font-sans",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Listly",
   description: "Share lists with your friends easily",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 export default function RootLayout({
   children,
 }: {
